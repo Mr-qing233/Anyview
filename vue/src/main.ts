9ace5725d6f43014c3d27fd921edf25d95bece4d
Mr-qing233 <2638353426@qq.com>
2022-09-11 20:30:41 +0800

    commit messge

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,6 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import request from "@/utils/request";
 
-createApp(App).use(store).use(router).mount("#app");
+
+const app=createApp(App).use(router).use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.config.globalProperties.request=request
+app.mount('#app')
